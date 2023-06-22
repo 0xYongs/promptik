6a9addb668be9b7901059cee51a7809feb30eb03
0xYongs <tradingsensei21@gmail.com>
2023-06-22 02:09:05 +0200

    Import styles/globals and add metadata of title and decription then rafce in layout.js in App folder

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    title: "Promptik",
+    description: 'Dicover & Share AI prompts'
+}
+
+const layout = () => {
+  return (
+    <div>layout</div>
+  )
+}
+
+export default layout
